c644462b28df0c2447e47d98cae4d60b359dfe63
Anton Astashov <35997240+ant00xa@users.noreply.github.com>
2021-01-25 09:16:50 +0300

    Update ReactDemoComponent.js

--- ReactDemoComponent.js
+++ ReactDemoComponent.js
@@ -1,2 +1,9 @@
-import React from 'react';import ReactDOM from 'react-dom';class Car extends React.Component{render(){return<h2>It's demo component</h2>}}
-export default Car
+import React from 'react';
+
+class DemoComponent extends React.Component {
+    render() {
+        return <h2>Demo component!</h2>;
+    }
+}
+
+export default DemoComponent;
