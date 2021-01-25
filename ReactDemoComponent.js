64f8f445d12a1eed019466940ef6ca61e9ee05d5
Anton Astashov <35997240+ant00xa@users.noreply.github.com>
2021-01-25 09:02:46 +0300

    Update ReactDemoComponent.js
    Minify

--- ReactDemoComponent.js
+++ ReactDemoComponent.js
@@ -1,10 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class Car extends React.Component {
-    render() {
-        return <h2>It's demo component</h2>;
-    }
-}
-
-export default Car;
+import React from 'react';import ReactDOM from 'react-dom';class Car extends React.Component{render(){return<h2>It's demo component</h2>}}
+export default Car
